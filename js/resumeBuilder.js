400a849efc116b178e826449a3232b0b67f7c696
GrecoMathster <GrecoMathster@users.noreply.github.com>
2015-01-11 00:09:38 +0000

    Update resumeBuilder.js

---
+++ js/resumeBuilder.js
@@ -0,0 +1,4 @@
+var formattedName = HTMLheaderName.replace("%data%" , "Ramin");
+var formattedRole = HTMLheaderRole.replace("%data%" , "Physicist, Mathematician, Revolutionary programmer");
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
